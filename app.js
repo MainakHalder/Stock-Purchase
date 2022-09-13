be29634f11fcb056fe99568f78fe7a9d40799c19
MainakHalder <76676017+MainakHalder@users.noreply.github.com>
2022-09-13 17:07:03 +0530

    corrected - formula

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ function submitHandler(){
 function calculateProfitAndLoss(initial, quantity, current){
     if(initial > current){
         var loss = (initial - current) * quantity;
-        var lossPercentage = (loss/initial) * 100;
+        var lossPercentage = loss/(initial * quantity) * 100;
         showOutput(`loss is ${loss} and the percentage is ${lossPercentage.toFixed(2)}%.`);
     }
     else if(initial < current){
         var profit = (current - initial) * quantity;
-        var profitPercentage = (profit/initial) * 100;
+        var profitPercentage = profit/(initial * quantity)  * 100;
         showOutput(`Profit is ${profit} and the percentage is ${profitPercentage.toFixed(2)}%.`);
     }
     else{
